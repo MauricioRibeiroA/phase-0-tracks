90c1f3e7f3c6b5127a1465b5abfc234be305fdab
MauricioRibeiroA <mauricioribeiro61@gmail.com>
2016-12-29 10:27:22 -0300

    updating file

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,14 +1,12 @@
 $new_array_of_integers = []
 array_of_integers = [42, 89, 23, 1, 24, 47, 35]
 
-
 def search(arr, number_to_search_for)
 x = 0
-until number_to_search_for == arr[x]
-$new_array_of_integers << arr[x]
-x += 1 
-
-end 
+   until number_to_search_for == arr[x]
+     $new_array_of_integers << arr[x]
+     x += 1 
+   end 
 $new_array_of_integers.length;
 end 
 
@@ -16,17 +14,40 @@ search(array_of_integers, 24)
 
 
 
-#arr.length = arr(100)
+def fibonacci(x)
+  arr = [0, 1]
+  y = x - 2 
+  y.times do 
+  sum = arr[-2] + arr[-1]
+  arr << sum 
+          end
+            return arr
+end 
 
-#sum the last to elements
+new = fibonacci(100)
+p new
 
-# add the new number into the arr
+=begin
+#I need to limit the loop with the same size of the array. I can use .length -1 for that.
+#The loop should take turns among the numbers, always considering the main "x" and the one after "x+1". 
+#Check a condition of true or false. If true change the main number with the one after and keep doing this until condition false. When false don't finish, but take the next number and do the same until condition false.
+#Iterate + loop will give me the condition to check each number until all "x" are in the right place.
+=end
+def method_sort(arr)
+  size = arr.length
+  loop do
+    trigger = false
+    (size-1).times do |x|
+      if arr[x] > arr[x+1]
+        arr[x], arr[x+1] = arr[x+1], arr[x]
+        trigger = true
+      end
+    end
 
-arr = [0, 1, 1, 2, 3, 5]
+    break if not trigger
+  end
 
-sum = arr[-2] + arr[-1]
+  arr
+end
 
-arr << sum 
-
-p arr
-
+method_sort([1, 3, 6, 2, 4, 5])
