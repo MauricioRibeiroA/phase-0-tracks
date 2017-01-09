c3f7b9b56308cabe2fa9cb078e0593aad522360e
MauricioRibeiroA <mauricioribeiro61@gmail.com>
2017-01-08 22:15:52 -0300

    commiting gps6 file

--- gps6/my_solution.rb
+++ gps6/my_solution.rb
@@ -10,29 +10,28 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+  #Defining the attributes of the VirusPredictor class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #Calls two other methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
-  private
 
-  def predicted_deaths(population_density, population, state)
+  private
+  #Prints the numbers of deaths within a state, depending on the population density
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
+    if @population_density > 200
       number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+    elsif @population_density <= 200 && @population_density >=50
+      number_of_deaths = (@population * ((@population_density.floor/50) * 0.1))
     else
       number_of_deaths = (@population * 0.05).floor
     end
@@ -40,23 +39,22 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
-  def speed_of_spread(population_density, state) #in months
+  # Returns the speed of the disease based on the population density
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
+if @population_density > 200
+      speed = (@population_density * 0.5)
+    elsif @population_density <= 200 && @population_density >=50
+      speed = (100/@population_density)
     else
       speed += 2.5
     end
+
+    
+    
+  
 
     puts " and will spread across the state in #{speed} months.\n\n"
 
@@ -70,18 +68,19 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+  STATE_DATA.each{ |state, state_info|
+    
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+    VirusPredictor.new(state,state_info[:population_density], state_info[:population]).virus_effects
+    }
 
 
 #=======================================================================
-# Reflection Section
+=begin
+Reflection Section
+Hash is shown encased in curly brackets, other hash is shown with arrow.
+Used to pair files with each other (within same folder). Different from require as we don't need full file path.
+Use a loop (.each, .times loop, for loops)
+Argument is not needed for virus_effects.
+=end
